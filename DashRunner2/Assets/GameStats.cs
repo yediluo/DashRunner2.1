2e0917746aa975c5efe13d7861184af2efe21f77
yediluo <luoyedi@126.com>
2019-07-16 19:50:15 -0700

    CoinCount Implemented

---
+++ DashRunner2/Assets/GameStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStats : MonoBehaviour
+{
+    [SerializeField] Image coin1;
+    [SerializeField] Image coin2;
+    [SerializeField] Image coin3;
+    public int CoinCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("coincount= " + CoinCount);
+        if(CoinCount > 2)
+        {
+            coin3.color = new Vector4(255, 255, 255, 255);
+            coin2.color = new Vector4(255, 255, 255, 255);
+            coin1.color = new Vector4(255, 255, 255, 255);
+
+        }
+        else if (CoinCount > 1) {
+            coin2.color = new Vector4(255, 255, 255, 255);
+            coin1.color = new Vector4(255, 255, 255, 255);
+
+
+        }
+        else if (CoinCount > 0) {
+
+            coin1.color = new Vector4(255, 255, 255, 255);
+        }
+    }
+}
--- DashRunner2/Assets/Script/coinPickUp.cs
+++ DashRunner2/Assets/Script/coinPickUp.cs
@@ -6,10 +6,19 @@ public class coinPickUp : MonoBehaviour
 {
     [SerializeField] Rigidbody2D myrb;
     [SerializeField] AudioClip PlayerBumpSFX;
-
+    [SerializeField] GameStats GS;
+    private void Start()
+    {
+        GS = FindObjectOfType<GameStats>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         AudioSource.PlayClipAtPoint(PlayerBumpSFX, transform.position);
+        GS.CoinCount++;
+
         Destroy(gameObject);
     }
+
+
+
 }
