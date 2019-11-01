10ca351c9e0852617264bc7ddef0e85968dcd084
yediluo <51844118+yediluo@users.noreply.github.com>
2019-10-31 17:53:02 -0700

    23 24 FINISH FIREBALL CAN TOUCH t1

--- DashRunner2/Assets/Script/Fireball.cs
+++ DashRunner2/Assets/Script/Fireball.cs
@@ -13,7 +13,7 @@ public class Fireball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Ground")|| collision.gameObject.CompareTag("Canno"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Ground")|| collision.gameObject.CompareTag("Canno")|| collision.gameObject.CompareTag("T1trap"))
         {
             Destroy(gameObject);
         }
